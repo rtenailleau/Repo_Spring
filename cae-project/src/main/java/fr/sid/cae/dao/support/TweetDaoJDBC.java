7f40ddae98219ace319b5ef86ef72fdba1901173
Srcvk <rvmail3@voila.fr>
2015-02-27 00:07:23 +0100

    ajout de l'envoi de message

--- cae-project/src/main/java/fr/sid/cae/dao/support/TweetDaoJDBC.java
+++ cae-project/src/main/java/fr/sid/cae/dao/support/TweetDaoJDBC.java
@@ -3,7 +3,10 @@ package fr.sid.cae.dao.support;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
 import fr.sid.cae.dao.TweetDao;
@@ -11,10 +14,26 @@ import fr.sid.cae.domain.Tweet;
 
 public class TweetDaoJDBC implements TweetDao {
 	
+	private static final Logger log = LoggerFactory.getLogger(TweetDao.class);
+	
+	private static final String GET_TWEETS = "SELECT * FROM tweet WHERE userid = ?";
+	private static final String ADD_TWEET  = "INSERT INTO tweet(id, userid, date, content) VALUES(?, ?, ?, ?)";
+	
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired	
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	@Override
+	public void addTweet(Tweet tweet) {
+		jdbcTemplate.update(ADD_TWEET, tweet.getId(), tweet.getUserId(), tweet.getDateDB(), tweet.getContent());	
+	}
+	
 	@Override
 	public List<Tweet> getUserTweet(String userId) {
-		// TODO Auto-generated method stub
-		return null;
+		return jdbcTemplate.query(GET_TWEETS, new TweetMapper(), userId);
 	}
 	
 	private class TweetMapper implements RowMapper<Tweet>{
--- cae-project/src/main/java/fr/sid/cae/domain/Tweet.java
+++ cae-project/src/main/java/fr/sid/cae/domain/Tweet.java
@@ -3,19 +3,69 @@ package fr.sid.cae.domain;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tweet")
 public class Tweet {
 	
 	private static final String textFormat = "dd/MM/yyyy HH:mm:ss";
 	private static final String dbFormat = "yyyyMMddHHmmss";
 	
+	@Id
+	@Column(name = "id", length = 36)
 	private String id;
+	@Column(name = "userid", length = 36)
 	private String userId;
+	@Column(name = "date")
 	private Date date = new Date();
+	@Column(name = "content")
 	private String content;
 	
-	private User user;
+	//private User user;
 	
 	public String getDateText(){
 		return new SimpleDateFormat(textFormat).format(date);
 	}
+	
+	public String getDateDB(){
+		return new SimpleDateFormat(dbFormat).format(date);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public Date getDate() {
+		return this.date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
 }
